5c4abe718b27b289c6b6918470d1d5eabd0e8714
marboe29 <52936913+marboe29@users.noreply.github.com>
2023-02-06 14:20:25 +1300

    Add files via upload
    Takes a function for each visualizer with a added function for chrome user audio policy

--- back.js
+++ back.js
@@ -1,17 +1,26 @@
+const outputSound = document.querySelector("audio");
+const canvasRain = document.getElementById("canvasRain")
+const ctx = canvasRain.getContext("2d");
 
-const oButton = document.getElementById("oneButton");
-const content = document.getElementById("content");
-const outputSound = document.getElementById("myAudio");
+const canvasSun = document.getElementById("canvasSun")
+const ctxSun = canvasSun.getContext("2d");
 
-const ctx = canvas.getContext("2d");
+const canvasLand = document.getElementById("canvasLand")
+const ctxLand = canvasLand.getContext("2d");
 
-WIDTH = 1200;
-HEIGHT = 600;
+WIDTH = 800;
+HEIGHT = 500;
 
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
+canvasRain.width = WIDTH;
+canvasRain.height = HEIGHT;
 
-const track = new Audio('music.mp3');
+canvasSun.width = WIDTH;
+canvasSun.height = HEIGHT;
+
+canvasLand.width = WIDTH;
+canvasLand.height = HEIGHT;
+
+const track = new Audio("music/Sugar.mp3");
 
 const audioCtx = new AudioContext();
 
@@ -21,67 +30,148 @@ const source = audioCtx.createMediaElementSource(track);
 
 source.connect(analyser);
 
-analyser.fftSize = 512;
+const rainArray = [];
 
-const bufferLength = analyser.frequencyBinCount;
-const dataArray = new Uint8Array(bufferLength);
+function defRain() {
 
-//console.log(dataArray);
-
-function draw(){
-    
-    const barWidth = canvas.width/bufferLength;
-    let barHeight;
-    let x = 0;
-    let x2 = canvas.width / 2;
-
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    const dataArray = new Uint8Array(analyser.frequencyBinCount);
 
     analyser.getByteFrequencyData(dataArray);
 
+    ctx.clearRect(0, 0, canvasRain.width, canvasRain.height);
+    
+    for (let i = 0; i < rainArray.length; i++) {
+    
+    rainArray[i].y += rainArray[i].dy - dataArray[i] / 50;
+      
+    ctx.beginPath()
+    ctx.roundRect(rainArray[i].x, rainArray[i].y, rainArray[i].width, rainArray[i].height,10)
+    ctx.stroke();
+
+    }
+    
+    requestAnimationFrame(defRain);
+    //console.log(dataArray);
+}
+
+function addRain() {
+
+    const width = Math.floor(Math.random() * 0) + 1;
+    const height = Math.floor(Math.random() * 10) + 8;
+    const x = Math.random() * (canvasRain.width - width);
+    const y = 0;
+    const dy = 2;
+  
+    const grad = ctx.createLinearGradient(0,0,0,canvasRain.height / 1);
+    grad.addColorStop(0,"#005182");
+    grad.addColorStop(0.5,"#ff4a6a");
+    grad.addColorStop(1,"white");
+  
+    const color = grad;
+    ctx.strokeStyle = color;
+    
+    rainArray.push({ x, y, width, height, dy, color });
+
+}
+
+defRain();
+
+function defSun() {
+
+    let bufferLength = analyser.frequencyBinCount /4;
+    let dataArray = new Uint8Array(bufferLength);
+    analyser.getByteFrequencyData(dataArray);
+    //console.log(bufferLength);
+    let barWidth = canvasSun.width/bufferLength;
+    let barHeight;
+    let x = 0;
+
+    ctxSun.clearRect(0,0,canvasSun.width,canvasSun.height);
+
     for (i = 0; i < bufferLength; i++){
 
-        barHeight = dataArray[i];
+        barHeight = dataArray[i] /2;
 
-        ctx.save();
-        ctx.translate(canvas.width/2,canvas.height/2);
-        ctx.rotate(i + Math.PI * 2 / bufferLength);
+        ctxSun.save();
+        ctxSun.translate(canvasSun.width/1.3,canvasSun.height/3);
+        ctxSun.rotate(i + Math.PI * 2 / bufferLength);
 
         const red = 255;
         const green = i * barHeight/10;
         const blue = 125;
         
-        ctx.fillStyle = "#FFF59E";
-        ctx.fillRect(0,canvas.height/8, barWidth, barHeight);
-        ctx.fillStyle = `rgb(`+red+`,`+green+`,`+blue+`)`;
-        ctx.fillRect(0,0, barWidth, barHeight);
+        ctxSun.fillStyle = "#FFF59E";
+        ctxSun.fillRect(0,canvasSun.height/8, barWidth, barHeight);
+        ctxSun.fillStyle = `rgb(`+red+`,`+green+`,`+blue+`)`;
+        ctxSun.fillRect(0,0, barWidth, barHeight);
 
         x += barWidth;
 
-        ctx.restore();
+        ctxSun.restore();
     }
 
-    requestAnimationFrame(draw);
+    requestAnimationFrame(defSun);
+
 }
 
-$(document).ready(function(){
+defSun();
 
-    $("#oneButton").toggle(
+function defSea() {
 
-        function(){$("#oneButton");
-        oButton.style.background = "green";
-        track.play();
-        outputSound.play();
-    },
-        function(){$("#oneButton");
-        oButton.style.background = "red";
-        track.pause();
-        outputSound.pause();
+    let bufferLength = analyser.frequencyBinCount /4;
+    let dataArray = new Uint8Array(bufferLength);
+    analyser.getByteFrequencyData(dataArray);
+    
+    let barWidth = canvasLand.width/bufferLength;
+    let barHeight;
+    let x = 0;
+    let x2 = canvasLand.width / 2;
+
+    ctxLand.clearRect(0,0,canvasLand.width,canvasLand.height);
+
+    for (i = 0; i < bufferLength; i++){
+
+        barHeight = dataArray[i];
+
+        let grad = ctx.createLinearGradient(0,0,0,canvasLand.height);
+        grad.addColorStop(0,"white");
+        grad.addColorStop(0.5,"#ff4a6a");
+        grad.addColorStop(1,"#44A7C4");
+        let color = grad;
+        //grass #83C760 sea #44A7C4
+
+        let width = Math.floor(Math.random() * 0) + 1;
+
+        ctxLand.beginPath()
+        ctxLand.roundRect(canvasLand.width /2 - x, canvasLand.height - barHeight /2, width, barHeight, 10)
+        ctxLand.roundRect(x2, canvasLand.height - barHeight /2, width, barHeight, 10)
+        ctxLand.stroke();
+
+        ctxLand.beginPath()
+        ctxLand.roundRect(canvasLand.width /1 - x, canvasLand.height - barHeight /2, width, barHeight, 10)
+        ctxLand.roundRect(x, canvasLand.height - barHeight /2, width, barHeight, 10)
+        ctxLand.stroke();
+
+        ctxLand.strokeStyle = color;
+
+        x += barWidth;
+        x2 += barWidth;
     }
-    );
-});
 
-draw();
+    requestAnimationFrame(defSea);
+}
 
+defSea();
 
-  
+//chrome user gesture policy
+function userGest() {
+
+    document.getElementById("userButton").innerHTML = "Running chrome";
+    if (audioCtx.state === 'suspended'){
+        audioCtx.resume();
+    }
+    track.play();
+    outputSound.play();
+    //adds rectangle every seconds
+    setInterval(addRain, 200);
+}
